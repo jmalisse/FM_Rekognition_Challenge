e087896aa2a605eaf0aac8fd26047d8fa3edd9bb
jmalisse <jeremy.malisse@gmail.com>
2020-04-02 22:49:02 -0400

    Added UI Color extension file

---
+++ FMACRekognitionChallenge/FMACRekognitionChallenge/Global-Common/Extensions/FMACColors.swift
@@ -0,0 +1,15 @@
+//
+//  FMACColors.swift
+//  FMACRekognitionChallenge
+//
+//  Created by Jeremy Malisse on 4/2/20.
+//  Copyright © 2020 Jeremy Malisse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+	static let fmac_blue = #colorLiteral(red: 0.2705882353, green: 0.6862745098, blue: 0.9137254902, alpha: 1)
+	static let fmac_green = #colorLiteral(red: 0.6, green: 0.7764705882, blue: 0.2274509804, alpha: 1)
+}
