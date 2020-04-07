f486b623efa1e79d3b78fc32d686812b1b772eff
jmalisse <jeremy.malisse@gmail.com>
2020-04-07 16:11:34 -0400

    Updated UIColor extension to better match company colors

--- FMACRekognitionChallenge/FMACRekognitionChallenge/Global-Common/Extensions/FMACColors.swift
+++ FMACRekognitionChallenge/FMACRekognitionChallenge/Global-Common/Extensions/FMACColors.swift
@@ -11,5 +11,5 @@ import UIKit
 
 extension UIColor {
 	static let fmac_blue = #colorLiteral(red: 0.2705882353, green: 0.6862745098, blue: 0.9137254902, alpha: 1)
-	static let fmac_green = #colorLiteral(red: 0.6, green: 0.7764705882, blue: 0.2274509804, alpha: 1)
+	static let fmac_green = #colorLiteral(red: 0.4666666667, green: 0.7529411765, blue: 0.1529411765, alpha: 1)
 }
