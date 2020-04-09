569cad2f691a3f569ec1baa522e9b18960f6948a
jmalisse <jeremy.malisse@gmail.com>
2020-04-09 14:42:38 -0400

    Removed debug UI code

--- FMACRekognitionChallenge/FMACRekognitionChallenge/Application/Modules/View Controllers/FMACViewController.swift
+++ FMACRekognitionChallenge/FMACRekognitionChallenge/Application/Modules/View Controllers/FMACViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 @IBDesignable class FMACViewController: ViewController {
 
+	@IBOutlet weak var continueButton: FMACUIButton!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,15 +28,4 @@ import UIKit
 			self.view.backgroundColor = backgroundColor
 		}
 	}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
